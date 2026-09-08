296c7e40dc211143f93c7622342500cbe63d7669
lu hao <luhao0606888@gmail.com>
2026-09-08 15:07:06 +0800

    no message

--- src/pages/common/pay.vue
+++ src/pages/common/pay.vue
@@ -9,18 +9,32 @@ export default {
     data() {
         return {
              targetUrl:'',
-             pay:0
+            
         };
     },
     onLoad(da){
-         this.pay = 1
-         this.targetUrl = da.url;
-         window.location.href = this.targetUrl
+        this.targetUrl = da.url
+        
+      
     },
     onShow(da){
-       if(this.pay){
-         MgtvApi.closeWebView();
-       }
+         if(!this.pay){
+            this.pay = 1
+         }else{
+
+         }
+         console.log(this.pay);
+       
+           console.log(window.location.search)
+           const params = new URLSearchParams(window.location.search);
+           console.log(params);
+           console.log(params.get('url'))
+        
+        //  window.location.href = this.targetUrl
+         
+    //    if(this.pay){
+    //      MgtvApi.closeWebView();
+    //    }
     }
    
 };
