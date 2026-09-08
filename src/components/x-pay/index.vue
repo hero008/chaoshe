68e0f3c94636f6058feb393ecfd8cc6e18cc739c
lu hao <luhao0606888@gmail.com>
2026-09-08 14:47:01 +0800

    no message

--- src/components/x-pay/index.vue
+++ src/components/x-pay/index.vue
@@ -982,7 +982,12 @@ export default {
         if(!res.code){
             that.$emit("success", res, that.showAnimation, click_type);
            that.close();
-           
+           console.log(window.location.origin);
+
+           if(window.MgtvApi){
+               jumpUrl(window.location.origin + '#/pages/common/pay?url='+res.res.createPaymentReply.payUrl)
+
+           }
           //  window.location.href =  res.res.createPaymentReply.payUrl
          
         }else{
---
+++ src/pages/common/pay.vue
@@ -0,0 +1,27 @@
+<template>
+    <view class="agreement">
+        
+    </view>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+             targetUrl:'',
+             pay:0
+        };
+    },
+    onLoad(da){
+         this.pay = 1
+         this.targetUrl = da.url;
+         window.location.href = this.targetUrl
+    },
+    onShow(da){
+       if(this.pay){
+         MgtvApi.closeWebView();
+       }
+    }
+   
+};
+</script>
