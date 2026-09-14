07b538baac9079c8a12e73b33533068413551b6d
lu hao <luhao0606888@gmail.com>
2026-09-14 10:05:08 +0800

    no message

--- src/utils/mgtv.js
+++ src/utils/mgtv.js
@@ -5,7 +5,7 @@ import BigNumber from "bignumber.js";
 
 export const isProd = process.env.NODE_ENV === "production"
 //  'https://mxs.mgworld.cn'
-export const originUrl= isProd? 'http://t8080.mgtv.com:8080':"http://t8080.mgtv.com:8080"
+export const originUrl= isProd? 'https://mxs.mgworld.cn':"http://t8080.mgtv.com:8080"
 
 export const  shareUrl = originUrl+'?isFullScreen=1&isHideNavBar=1'
 export const isMTVapp = () => {
