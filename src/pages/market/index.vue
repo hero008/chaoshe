3635dbaa2e1bf6c87c37a41d10e4ed8233178160
lu hao <luhao0606888@gmail.com>
2026-09-20 14:42:44 +0800

    no message

--- src/pages/market/index.vue
+++ src/pages/market/index.vue
@@ -450,8 +450,8 @@ export default {
                              
                         // }
                     
-                       
-                        res.order.map((i) => { i.state = getApp().globalData.MarketOrderState[i.state]; });
+                        res.order.state = getApp().globalData.MarketOrderState[res.order.state]
+                        // res.order.map((i) => { i.state = getApp().globalData.MarketOrderState[i.state]; });
                         this.orderList.splice(this.oldInx.inx, 1, res.order);
                     }
                 });
